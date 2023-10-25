b5db5575ae512f098acbb0c3ce57bb8ec3f0e16c
wen zhou <zhouwenh@amazon.com>
2023-10-25 15:32:07 +0800

    demo test

---
+++ AppTest/AccountTest/ServiceTest/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "example_server" {
+  ami           = "ami-0b0f138edf421d756"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Test"
+  }
+}
---
+++ AppTest/AccountTest/ServiceTest/versions.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+}
