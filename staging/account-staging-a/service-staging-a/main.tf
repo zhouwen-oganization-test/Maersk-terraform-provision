c460d030c07187c6b3453e42d7683f49f9cc0564
wen zhou <zhouwenh@amazon.com>
2023-11-02 10:57:30 +0800

    update ec2 provision blocks

--- staging/account-staging-a/service-staging-a/main.tf
+++ staging/account-staging-a/service-staging-a/main.tf
@@ -1,8 +1,14 @@
-resource "aws_instance" "example_server" {
-  ami           = "ami-0b0f138edf421d756"
-  instance_type = "t2.micro"
+module "ec2_instance" {
+  source  = "git@github.com:bitzhouwen/Maersk-terraform-modules.git//terraform-aws-ec2-instance"
+
+  name = "staging-a-service-a-prototype-instances"
+
+  ami                    = "ami-0b0f138edf421d756"
+  instance_type          = "t3.micro"
+  monitoring             = false 
 
   tags = {
-    Name = "terraform test service staging-a test branch v2"
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
