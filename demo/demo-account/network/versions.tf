e061486f1abe5fcd9f7aa1e7143b99a53190c209
wen zhou <zhouwenh@amazon.com>
2023-11-20 10:13:35 +0800

    try new workflow

---
+++ demo/demo-account/network/versions.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "ap-southeast-1"
+  assume_role {
+    role_arn = "arn:aws:iam::129980683758:role/AdminRole"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+  required_version = ">= 1.5.0"
+
+  backend "s3" {
+    key            = "demo/demo-account/network.tfstate"
+    bucket         = "maersk-remote-tfstate-poc"
+    dynamodb_table = "maersk-remote-state-lock"
+    region         = "ap-southeast-1"
+  }
+
+}
