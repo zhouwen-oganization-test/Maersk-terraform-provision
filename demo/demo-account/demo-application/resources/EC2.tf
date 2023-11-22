de1ec9808a0540b1b95c5d49e81241b5ba181893
wen zhou <zhouwenh@amazon.com>
2023-11-22 17:30:18 +0800

    update ec2

--- demo/demo-account/demo-application/resources/EC2.tf
+++ demo/demo-account/demo-application/resources/EC2.tf
@@ -1,13 +1,6 @@
 locals {
   config_name = "config/EC2.yaml"
-  yaml_vars = yamldecode(file("${path.module}/${local.config_name}"))
-}
-
-locals {
-
-  # accounts_config = { for a in local.yaml_vars.accounts : a.account_alias => a }
-  account_config  = local.yaml_vars
-
+  account_config = yamldecode(file("${path.module}/${local.config_name}"))
   templates_config = { for t in try(local.yaml_vars.templates, []) : t.name => t }
 }
 
@@ -22,7 +15,7 @@ variable "windows_pam_user_password" {
 module "tag_defs" {
   # source     = "/Users/zhouwenh/Desktop/maersk/maersk-code/Maersk-org-terraform-modules/terraform-modules-ec2-demo/tag-defs"
   source     = "git::https://github.com/zhouwen-oganization-test/Maersk-org-terraform-modules.git//terraform-modules-ec2-demo/tag-defs?ref=main"
-  tag_defs   = try(local.yaml_vars.tag_defs, [])
+  tag_defs   = try(local.account_config.tag_defs, [])
 }
 
 module "ec2_instances" {
@@ -31,7 +24,6 @@ module "ec2_instances" {
   account_config            = local.account_config
   templates_config          = local.templates_config
   tag_defs_template         = module.tag_defs.template
-#   account_alias_id_map      = data.terraform_remote_state.org.outputs.account_alias_id_map
   account_alias_id_map      = {}
   windows_pam_user_password = var.windows_pam_user_password
 }
