4af383eb3a9984f847be5aa66a8b5315b851b355
wen zhou <zhouwenh@amazon.com>
2023-11-23 22:42:09 +0800

    modify modules

--- demo/demo-account/demo-application/resources/EC2.tf
+++ demo/demo-account/demo-application/resources/EC2.tf
@@ -1,7 +1,6 @@
 locals {
   config_name = "config/EC2.yaml"
   account_config = yamldecode(file("${path.module}/${local.config_name}"))
-  templates_config = { for t in try(local.account_config.templates, []) : t.name => t }
 }
 
 data "aws_partition" "this" {}
@@ -12,18 +11,17 @@ variable "windows_pam_user_password" {
   default = "RotateMe"
 }
 
-module "tag_defs" {
-  # source     = "/Users/zhouwenh/Desktop/maersk/maersk-code/Maersk-org-terraform-modules/terraform-modules-ec2-demo/tag-defs"
-  source     = "git::https://github.com/zhouwen-oganization-test/Maersk-org-terraform-modules.git//terraform-modules-ec2-demo/tag-defs?ref=main"
-  tag_defs   = try(local.account_config.tag_defs, [])
-}
+# module "tag_defs" {
+#   # source     = "/Users/zhouwenh/Desktop/maersk/maersk-code/Maersk-org-terraform-modules/terraform-modules-ec2-demo/tag-defs"
+#   source     = "git::https://github.com/zhouwen-oganization-test/Maersk-org-terraform-modules.git//terraform-modules-ec2-demo/tag-defs?ref=main"
+#   tag_defs   = try(local.account_config.tag_defs, [])
+# }
 
 module "ec2_instances" {
   # source                    = "/Users/zhouwenh/Desktop/maersk/maersk-code/Maersk-org-terraform-modules/terraform-modules-ec2-demo"
   source                    = "git::https://github.com/zhouwen-oganization-test/Maersk-org-terraform-modules.git//terraform-modules-ec2-demo?ref=main"
   account_config            = local.account_config
   templates_config          = local.templates_config
-  tag_defs_template         = module.tag_defs.template
   account_alias_id_map      = {}
   windows_pam_user_password = var.windows_pam_user_password
 }
