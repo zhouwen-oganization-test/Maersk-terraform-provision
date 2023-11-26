a93c22dac4113f37c719bc53cb0d42a1b102471f
wen zhou <zhouwenh@amazon.com>
2023-11-27 00:36:10 +0800

    update codes

--- demo/demo-account/demo-application/resources/ebs.tf
+++ demo/demo-account/demo-application/resources/ebs.tf
@@ -1,13 +1,13 @@
 locals {
   ebs_config_name      = "config/ebs.yaml"
-  ebs_instance_config  = yamldecode(file("${path.module}/${local.ebs_config_name}"))
-  ebs_templates_config = { for t in try(local.instance_config.templates, []) : t.name => t }
+  ebs_config  = yamldecode(file("${path.module}/${local.ebs_config_name}"))
+  ebs_templates_config = { for t in try(local.ebs_config.templates, []) : t.name => t }
 }
 
 module "ebs_volumes" {
   # source                    = "/Users/zhouwenh/Desktop/maersk/maersk-code/Maersk-org-terraform-modules/terraform-modules-ebs-demo"
   source                    = "git::https://github.com/zhouwen-oganization-test/Maersk-org-terraform-modules.git//terraform-modules-ebs?ref=main"
-  instance_config           = local.ebs_instance_config
+  instance_config           = local.ebs_config
   templates_config          = local.ebs_templates_config
 
 }
--- demo/demo-account/demo-application/resources/ec2.tf
+++ demo/demo-account/demo-application/resources/ec2.tf
@@ -1,7 +1,7 @@
 locals {
   ec2_config_name     = "config/ec2.yaml"
-  ec2_instance_config = yamldecode(file("${path.module}/${local.ec2_config_name}"))
-  templates_config    = { for t in try(local.ec2_instance_config.templates, []) : t.name => t }
+  ec2_config = yamldecode(file("${path.module}/${local.ec2_config_name}"))
+  ec2_templates_config    = { for t in try(local.ec2_config.templates, []) : t.name => t }
 }
 
 variable "windows_pam_user_password" {
@@ -11,7 +11,7 @@ variable "windows_pam_user_password" {
 module "ec2_instances" {
   # source                    = "/Users/zhouwenh/Desktop/maersk/maersk-code/Maersk-org-terraform-modules/terraform-modules-ec2"
   source                    = "git::https://github.com/zhouwen-oganization-test/Maersk-org-terraform-modules.git//terraform-modules-ec2?ref=main"
-  instance_config           = local.ec2_instance_config
+  instance_config           = local.ec2_config
   templates_config          = local.ec2_templates_config
   # account_alias_id_map      = {}
   windows_pam_user_password = var.windows_pam_user_password
