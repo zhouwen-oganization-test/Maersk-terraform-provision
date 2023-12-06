bb32255169743c600ef9360e15314c4b9165ddca
wen zhou <zhouwenh@amazon.com>
2023-12-06 18:24:26 +0800

    update fsx codes

---
+++ demo/demo-account/demo-application/fsx/fsx.tf
@@ -0,0 +1,16 @@
+locals {
+  fsx_config_name     = "config/fsx.yaml"
+  fsx_config = yamldecode(file("${path.module}/${local.fsx_config_name}"))
+}
+
+variable "FSX_ADMIN_PASSWORD" {
+  description = "get password from github environment secrets"
+  default = "RotateMe"
+}
+
+module "fsx-instances" {
+  # source                    = "/Users/zhouwenh/Desktop/maersk/maersk-code/Maersk-org-terraform-modules/terraform-modules-fsx-ontap"
+  source                    = "git::https://github.com/zhouwen-oganization-test/Maersk-org-terraform-modules.git//terraform-modules-fsx-ontap?ref=main"
+  fsx_config           = local.fsx_config
+  fsx_admin_password   = var.FSX_ADMIN_PASSWORD
+}
