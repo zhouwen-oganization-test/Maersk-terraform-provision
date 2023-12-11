b0032c6cc0a1aefddce3c5cbc246b1d45fc8da2a
Jiawei Wang <wajiawei@amazon.com>
2023-12-11 12:03:28 +0800

    opensearch

---
+++ demo/demo-account/demo-application/opensearch/opensearch.tf
@@ -0,0 +1,16 @@
+locals {
+  opensearch_config_name     = "config/opensearch.yaml"
+  opensearch_config = yamldecode(file("${path.module}/${local.opensearch_config_name}"))
+}
+
+variable "master_user_password" {
+  description = "get password from github environment secrets"
+  default = ""
+}
+
+module "fsx-instances" {
+  # source                    = "/Users/zhouwenh/Desktop/maersk/maersk-code/Maersk-org-terraform-modules/terraform-modules-fsx-ontap"
+  source                    = "git::https://github.com/zhouwen-oganization-test/Maersk-org-terraform-modules.git//terraform-modules-opensearch?ref=main"
+  opensearch_config           = local.opensearch_config
+  master_user_password   = var.master_user_password
+}
